8e48bb496a4fdf8e2a261476f54e5e20450e6810
Rafael Calearo <rafaelcalearo@hotmail.com>
2018-05-08 15:22:04 -0300

    Primeiro commit :)

---
+++ src/main/java/com/theeventsspringboot/TheeventsspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.theeventsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheeventsspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheeventsspringbootApplication.class, args);
+	}
+}
