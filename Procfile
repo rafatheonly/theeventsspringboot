09d07c7b7381c64413442415395ecace9575a08c
Rafael Calearo <rafaelcalearo@hotmail.com>
2018-05-08 15:15:05 -0300

    Zerando o repo!

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Dserver.port=$PORT -jar  target/theeventsspringboot-0.0.1-SNAPSHOT.jar -Dspring.profiles.active=prod
--- src/main/resources/outros/importEvento.sql
+++
@@ -1,6 +0,0 @@
-insert into evento (titulo, data, descricao) values ('Encontro de Carros', 'Seg 18 Mai 2018', 'Praesent tincidunt sed tellus ut rutrum sed vitae justo...');
-insert into evento (titulo, data, descricao) values ('Festival de Rua', 'Seg 18 Mai 2019', 'Praesent tincidunt sed tellus ut rutrum sed vitae justo...');
-insert into evento (titulo, data, descricao) values ('Passeio em Veneza', 'Seg 18 Mai 2020', 'Praesent tincidunt sed tellus ut rutrum sed vitae justo...');
-insert into evento (titulo, data, descricao) values ('Teste01', 'Seg 18 Mai 2021', 'Praesent tincidunt sed tellus ut rutrum sed vitae justo...');
-insert into evento (titulo, data, descricao) values ('Teste02', 'Seg 18 Mai 2021', 'Praesent tincidunt sed tellus ut rutrum sed vitae justo...');
-
