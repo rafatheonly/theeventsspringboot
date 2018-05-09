4b6f62e9ead739f04cbc969cc820376e1dcb553d
Rafael Calearo <rafaelcalearo@hotmail.com>
2018-05-09 16:14:50 -0300

    Adicionando insert na migration!

--- src/main/resources/db/migration/V1__Create_evento_table.sql
+++ src/main/resources/db/migration/V1__Create_evento_table.sql
@@ -9,3 +9,4 @@ create table EVENTO (
 insert into evento (TITULO, DATA, DESCRICAO, FOTO) values ('Encontro de Carros', 'Seg 18 Mai 2018', 'Praesent tincidunt sed tellus ut rutrum sed vitae justo...', '1.jpg');
 insert into evento (TITULO, DATA, DESCRICAO, FOTO) values ('Festival de Rua', 'Seg 18 Mai 2019', 'Praesent tincidunt sed tellus ut rutrum sed vitae justo...', '2.jpg');
 insert into evento (TITULO, DATA, DESCRICAO, FOTO) values ('Passeio em Veneza', 'Seg 18 Mai 2020', 'Praesent tincidunt sed tellus ut rutrum sed vitae justo...', '3.jpg');
+insert into evento (TITULO, DATA, DESCRICAO, FOTO) values ('Travessia', 'Seg 18 Mai 2020', 'Praesent tincidunt sed tellus ut rutrum sed vitae justo...', '4.jpg');
