423ec4dc64d283f3179256e2c4674bc5459f41e5
Rafael Calearo <rafaelcalearo@hotmail.com>
2018-06-04 16:54:57 -0300

    Zerando o repositório!

--- outros/db/migration/postgresql/V0002__Create_usuario_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE USUARIO(
-	ID serial primary key not null ,
-	nome VARCHAR(100) not null,
-	cpf VARCHAR(30) not null,
-	email VARCHAR(70) not null,
-	senha VARCHAR(150) NOT NULL,
-	foto VARCHAR(50),
-	ativo BOOLEAN not null
-);
