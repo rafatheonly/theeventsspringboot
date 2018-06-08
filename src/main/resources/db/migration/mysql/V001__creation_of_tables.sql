658900b2ecce3c16a3fdb31eec4b1372bc4776ee
Rafael Calearo <rafaelcalearo@hotmail.com>
2018-06-08 16:26:46 -0300

    Alteração nas migrations de criação e inserção do Flyway!

--- src/main/resources/db/migration/mysql/V001__creation_of_tables.sql
+++ src/main/resources/db/migration/mysql/V001__creation_of_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE usuario (
-  id BIGINT(1) NOT NULL AUTO_INCREMENT,
+  id BIGINT(1) NOT NULL,
   nome VARCHAR(45) NOT NULL,
   rg VARCHAR(10) NOT NULL,
   email VARCHAR(45) NOT NULL,
@@ -11,15 +11,15 @@ CREATE TABLE usuario (
 ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE tipoevento (
-  id BIGINT(1) NOT NULL AUTO_INCREMENT,
+  id BIGINT(1) NOT NULL,
   descricao_tipo_evento VARCHAR(45) NOT NULL,
   usuario_id BIGINT(1) NOT NULL,
   PRIMARY KEY (id, usuario_id), 
-    FOREIGN KEY (usuario_id) REFERENCES usuario (id))
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id))
 ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE evento (
-  id BIGINT(1) NOT NULL AUTO_INCREMENT,
+  id BIGINT(1) NOT NULL,
   usuario_id BIGINT(1) NOT NULL,
   tipoevento_id BIGINT(1) NOT NULL,
   titulo VARCHAR(45) NOT NULL,
@@ -29,29 +29,29 @@ CREATE TABLE evento (
   foto VARCHAR(60) NULL,
   ativo TINYINT(1) NOT NULL,
   PRIMARY KEY (id, usuario_id, tipoevento_id), 
-    FOREIGN KEY (usuario_id) REFERENCES usuario (id), 
-    FOREIGN KEY (tipoevento_id) REFERENCES tipoevento (id))
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id), 
+    FOREIGN KEY (tipoevento_id) REFERENCES tipoevento(id))
 ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE convidado (
-  id BIGINT(1) NOT NULL AUTO_INCREMENT,
+  id BIGINT(1) NOT NULL,
   nome VARCHAR(45) NOT NULL,
   rg VARCHAR(10) NULL,
   evento_id BIGINT(1) NOT NULL,
   usuario_id BIGINT(1) NOT NULL,
   PRIMARY KEY (id, evento_id, usuario_id),  
-    FOREIGN KEY (evento_id) REFERENCES evento (id), 
-    FOREIGN KEY (usuario_id) REFERENCES usuario (id))
+    FOREIGN KEY (evento_id) REFERENCES evento(id), 
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id))
 ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE comentario (
-  id BIGINT(1) NOT NULL AUTO_INCREMENT,
+  id BIGINT(1) NOT NULL,
   usuario_id BIGINT(1) NOT NULL,
   evento_id BIGINT(1) NOT NULL,
   data_comentario VARCHAR(45) NOT NULL,
   comentario VARCHAR(200) NOT NULL,
   ativo TINYINT(1) NOT NULL,
   PRIMARY KEY (id, usuario_id, evento_id),  
-    FOREIGN KEY (usuario_id) REFERENCES usuario (id),  
-    FOREIGN KEY (evento_id) REFERENCES evento (id))
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id),  
+    FOREIGN KEY (evento_id) REFERENCES evento(id))
 ENGINE = InnoDB DEFAULT CHARSET=utf8;
